65f278972792be556bf52323e2dda57003b435c8
golen35 <bellyfragao@gmail.com>
2024-11-08 11:25:54 +0100

    Updates fotos

--- src/controller/CrudsControllers.js
+++ src/controller/CrudsControllers.js
@@ -1,5 +1,8 @@
 import asyncHandler from 'express-async-handler'
+import formidable from 'formidable';
 import User from "../model/UserModel"
+
+const _= require("lodash");
 
 // Display list of all Users.
 exports.UsersControllers = asyncHandler(async (req, res, next) => {
@@ -23,6 +26,25 @@ exports.CreateControllers = asyncHandler(async (req, res, next) => {
         if(userExist){
           return res.status(400).json({message: "User already exists."})
         }
+        // Objects Formidable
+        const form = new formidable.IncomingForm()
+        form.keepExtensions = true
+        form.parse(req, (err, fields, files) => {
+          if(err) {
+            return res.status(400).json({
+              error: "Image could not be uploaded"
+            })
+          }
+          //
+          const Usepoto = new User(fields)
+          if(files.photo) {
+            Usepoto.photo.data = fs.readFileSync(files.photo.path)
+            Usepoto.photo.contenType = files.photo.type
+          }
+        })
+
+       
+
         const savedUser = await userData.save();
         res.status(200).json(savedUser);
     } catch (error) {
